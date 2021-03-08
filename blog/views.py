e3d79b0497b8bd38f4f8d9b962107352caaf351e
mrusert <michael.rusert@gmail.com>
2021-03-07 17:13:38 -0700

    Added comment moderation

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
-from .models import Post # the . means current directory or current application
+from .models import Post, Comment # the . means current directory or current application
 from .forms import PostForm, CommentForm
 from django.contrib.auth.decorators import login_required
 
@@ -75,3 +75,15 @@ def add_comment_to_post(request, pk):
     else:
         form = CommentForm()
     return render(request, 'blog/add_comment_to_post.html', {'form': form})
+
+@login_required
+def comment_approve(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    comment.approve()
+    return redirect('post_detail', pk=comment.post.pk)
+
+@login_required
+def comment_remove(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    comment.delete()
+    return redirect('post_detail', pk=comment.post.pk)
